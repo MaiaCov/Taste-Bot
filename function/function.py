fc0098f103c29c7acdff71010e31fbb4f8f3247b
Olesya Goroshkova <goroskovao@uni.coventry.ac.uk>
2021-11-08 15:09:44 +0200

    function output updated

--- function/function.py
+++ function/function.py
@@ -24,15 +24,26 @@ def nonAlcoCocktail(flav1): # maybe add search by at least one word like 'coffee
     i=0
     for i in range (lengh): # storing all drinks id data to the list
         drinkByAlco.append(json.dumps((dataNonAlco["drinks"][i])["idDrink"], indent=4))
-    finalDrinkOptions = [x for x in drinkByFlav if x in drinkByAlco]   # finalDrinkOptions = set(drinkByFlav).intersection(drinkByAlco) - I know about this method, but I can't use it due to fron data format on input
-    drink = choice(finalDrinkOptions) # random choice of final drink from all options that was found
-    # print(drink) - you will receive drink id
-    return True
+    finalDrinkOptions = [x for x in drinkByFlav if x in drinkByAlco]   #finalDrinkOptions = set(drinkByFlav).intersection(drinkByAlco) - I know about this method, but I can't use it due to fron data format on input
+    drinkID = choice(finalDrinkOptions) 
+    drinkID = drinkID.replace('"', '')
+    # random choice of final drink from all options that was found
+    moreData = requests.get(f'https://thecocktaildb.com/api/json/v1/1/lookup.php?i={drinkID}').json()
+    drink = (moreData["drinks"])[0]
+    # maybe we can show ingredients to user because it was a random choice, like only name isn't enough
+    ingredients = drink["strIngredient1"] + ", " + drink["strIngredient2"] 
+    if type(drink["strIngredient3"]) != type(None):
+      ingredients = ingredients + ", " + (drink["strIngredient3"])
+      if  type(drink["strIngredient4"]) != type(None):
+           ingredients = ingredients + ", " + (drink["strIngredient4"])
+           if  type(drink["strIngredient5"]) != type(None):
+              ingredients = ingredients + ", " + (drink["strIngredient5"])
+    result = drink["strDrink"], ingredients
+    return result
 
 def random_cocktail():
     data = requests.get('https://thecocktaildb.com/api/json/v1/1/random.php').json() 
-    drink = (data["drinks"][0])
-    drinkID = (data["drinks"][0])["idDrink"]
+    drink = (data["drinks"])[0]
     # maybe we can show ingredients to user because it was a random choice, like only name isn't enough
     ingredients = drink["strIngredient1"] + ", " + drink["strIngredient2"] 
     if type(drink["strIngredient3"]) != type(None):
@@ -41,11 +52,10 @@ def random_cocktail():
            ingredients = ingredients + ", " + (drink["strIngredient4"])
            if  type(drink["strIngredient5"]) != type(None):
                ingredients = ingredients + ", " + (drink["strIngredient5"])
-    #print(drinkID)
-    #print (ingredients)
-    data = requests.get('https://thecocktaildb.com/api/json/v1/1/random.php').json() 
-    drink = choice(data["drinks"]) 
-    return drink
+    #drink = drink["strDrink"] + drink["idDrink"] + ingredients
+    result = drink["strDrink"], ingredients
+    return result
+    
 
 def flavor_cocktail(flav1, flav2):
     data = requests.get(f'https://thecocktaildb.com/api/json/v1/1/filter.php?i={flav1}').json()
@@ -53,13 +63,24 @@ def flavor_cocktail(flav1, flav2):
     i=0
     finalDrinkOptions = []
     for i in range (lengh):
-        # drinkID = (data["drinks"][i])["idDrink"]
+        # drink ID = (data["drinks"][i])["idDrink"]
         moreData = requests.get (f'https://thecocktaildb.com/api/json/v1/1/lookup.php?i={(data["drinks"][i])["idDrink"]}').json()
         if flav2 == (moreData["drinks"][0])["strIngredient1"] or flav2 == (moreData["drinks"][0])["strIngredient2"] or flav2 == (moreData["drinks"][0])["strIngredient3"] or flav2 == (moreData["drinks"][0])["strIngredient4"] or flav2 == (moreData["drinks"][0])["strIngredient5"] or flav2 == (moreData["drinks"][0])["strIngredient6"]:
             finalDrinkOptions.append(json.dumps((data["drinks"][i])["idDrink"], indent=4))
-            drink = choice(finalDrinkOptions)  # random choice of final drink from all options that was found
-            # print (drink)
-    return True 
+            drinkID = choice(finalDrinkOptions)  # random choice of final drink from all options that was found
+    drinkID = drinkID.replace('"', '')
+    moreData = requests.get(f'https://thecocktaildb.com/api/json/v1/1/lookup.php?i={drinkID}').json()
+    drink = (moreData["drinks"])[0]
+    # maybe we can show ingredients to user because it was a random choice, like only name isn't enough
+    ingredients = drink["strIngredient1"] + ", " + drink["strIngredient2"] 
+    if type(drink["strIngredient3"]) != type(None):
+      ingredients = ingredients + ", " + (drink["strIngredient3"])
+      if  type(drink["strIngredient4"]) != type(None):
+           ingredients = ingredients + ", " + (drink["strIngredient4"])
+           if  type(drink["strIngredient5"]) != type(None):
+              ingredients = ingredients + ", " + (drink["strIngredient5"])
+    result = drink["strDrink"], ingredients
+    return (result)
 
     # ina = '"' + (f"strIngredient{a}") + '"'
     # a=1 ... and then 2, 3, 4,...
